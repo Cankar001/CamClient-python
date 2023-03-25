589c89e70748fcb49a43ce9d82caa7f9cbee81a9
Can Karka <53074877+Cankar001@users.noreply.github.com>
2023-03-25 19:20:35 +0100

    Added first multi-processing try

--- Client.py
+++ Client.py
@@ -1,7 +1,6 @@
 import math
+import queue
 import socket
-import threading
-from threading import Lock
 import time
 import cv2
 
@@ -16,13 +15,9 @@ import struct
 
 import Logger
 import EnvironmentLoader
+from multiprocessing import Pool, Queue
 
 usleep = lambda x: time.sleep(x/1000000.0)
-
-threads = []
-names = []
-pick = []
-faceLocations = []
 
 def send(c: socket.socket, msg: str):
     """
@@ -85,26 +80,26 @@ def leave(c, motion_detected):
 
     time.sleep(1)
 
-def worker(threadLock, rgb, knownFaceEncodings, knownPersons):
-    global faceLocations
-    global names
-    global pick
+def init_pool(db, rb):
+    global detection_buffer
+    global render_buffer
+    detection_buffer = db
+    render_buffer = rb
 
-    threadLock.acquire()
-    faceLocations = face_recognition.face_locations(rgb)
-    threadLock.release()
+def detect_object(render_frame, image, knownFaceEncodings, knownPersons, aspect):
+    # initialize the HOG descriptor/person detector
+    hog = cv2.HOGDescriptor()
+    hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 
-    face_encodings = face_recognition.face_encodings(rgb, faceLocations)
+    face_locations = face_recognition.face_locations(image)
+    face_encodings = face_recognition.face_encodings(image, face_locations)
 
     # pedestrian detection
-    (rects, weights) = hog.detectMultiScale(rgb, winStride=(4, 4),
+    (rects, weights) = hog.detectMultiScale(render_frame, winStride=(4, 4),
                                             padding=(8, 8), scale=1.05)
 
     rects = np.array([[x, y, x + w, y + h] for (x, y, w, h) in rects])
-
-    threadLock.acquire()
     pick = non_max_suppression(rects, probs=None, overlapThresh=0.65)
-    threadLock.release()
 
     name = 'UNKNOWN'
     encoding_names = []
@@ -127,11 +122,64 @@ def worker(threadLock, rgb, knownFaceEncodings, knownPersons):
             # of votes (note: in the event of an unlikely tie Python
             # will select first entry in the dictionary)
             name = max(counts, key=counts.get)
-            encoding_names.append(name)
+        encoding_names.append(name)
 
-    threadLock.acquire()
-    names = encoding_names
-    threadLock.release()
+    result = {
+        'render_frame': render_frame,
+        'names': encoding_names,
+        'pick': pick,
+        'aspect': aspect,
+        'face_locations': face_locations
+    }
+
+    Logger.success('Worker task was successful')
+    detection_buffer.put(result)
+
+def show():
+    while True:
+        data = detection_buffer.get()
+        if data is None:
+            break
+
+        Logger.success('Received frame from worker...')
+
+        current_render_frame = data['render_frame']
+        current_names = data['names']
+        current_pick = data['pick']
+        current_aspect = data['aspect']
+        current_face_locations = data['face_locations']
+
+        # loop over the recognized faces
+        for ((top, right, bottom, left), name) in zip(current_face_locations, current_names):
+            # rescale the face coordinates
+            top = int(top * current_aspect)
+            right = int(right * current_aspect)
+            bottom = int(bottom * current_aspect)
+            left = int(left * current_aspect)
+
+            # draw the predicted face name on the image
+            cv2.rectangle(current_render_frame, (left, top), (right, bottom), (0, 255, 0), 2)
+            y = top - 15 if top - 15 > 15 else top + 15
+            cv2.putText(current_render_frame, name, (left, y), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 255, 0), 2)
+
+        # draw the final bounding boxes
+        for (xA, yA, xB, yB) in current_pick:
+            cv2.rectangle(current_render_frame, (xA, yA), (xB, yB), (0, 255, 0), 2)
+
+        print(f'Face locations: {current_face_locations}')
+        print(f'Names: {current_names}')
+        print(f'Picks: {current_pick}')
+
+        detected = len(current_names) > 0 or len(current_pick) > 0
+
+        result = {
+            'detected': detected,
+            'frame': current_render_frame,
+        }
+
+        Logger.success('Frame successfully post-processed')
+        detection_buffer.task_done()
+        render_buffer.put(result)
 
 
 if __name__ == '__main__':
@@ -164,10 +212,6 @@ if __name__ == '__main__':
             # DEBUG
             raise e
 
-    # initialize the HOG descriptor/person detector
-    hog = cv2.HOGDescriptor()
-    hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
-
     # now open the video capture device
     vid = cv2.VideoCapture(0)
     vid.set(cv2.CAP_PROP_CONVERT_RGB, 1.0)
@@ -176,56 +220,48 @@ if __name__ == '__main__':
     frame_height = int(vid.get(4))
     join(client, frame_width, frame_height)
 
-    threadLock = Lock()
+    # Create a worker thread that should do all heavy calculations
+    detection_buffer = Queue()
+    render_buffer = Queue()
+
+    # 6 workers: 1 for the show task and 5 to process frames:
+    pool = Pool(6, initializer=init_pool, initargs=(detection_buffer, render_buffer))
+    # run the "show" task:
+    show_process = pool.apply_async(show, args=())
+
+    frame_workers = []
+    anything_detected = False
+    last_frame_for_server = None
     while vid.isOpened():
         try:
-            ret, frame = vid.read()
-            if not ret:
+            success, frame = vid.read()
+            if not success:
                 break
 
-            start_time = time.time()
-
+            anything_detected = False
             rgb = imutils.resize(frame, width=250)
-            r = frame.shape[1] / float(rgb.shape[1])
+            aspect_ratio = frame.shape[1] / float(rgb.shape[1])
 
-            # Create a worker thread that should do all heavy calculations
-            current_worker_thread = threading.Thread(target=worker, args=[threadLock, rgb, knownFaceEncodings, knownPersons])
-            threads.append(current_worker_thread)
-            current_worker_thread.start()
+            f = pool.apply_async(detect_object, args=[frame, rgb, knownFaceEncodings, knownPersons, aspect_ratio])
+            frame_workers.append(f)
 
-            # wait a small amount to allow the worker thread a head start
-            time.sleep(0.0035)
+            try:
+                render_data = render_buffer.get(block=False)
+                anything_detected = render_data['detected']
+                frame_for_server = render_data['frame']
+                last_frame_for_server = frame_for_server
 
-            print(f'Face locations: {faceLocations}')
-            print(f'Names: {names}')
-            print(f'Picks: {pick}')
+                # send the image to the server
+                send_image_data(client, frame_for_server)
 
-            # loop over the recognized faces
-            for ((top, right, bottom, left), name) in zip(faceLocations, names):
-                # rescale the face coordinates
-                top = int(top * r)
-                right = int(right * r)
-                bottom = int(bottom * r)
-                left = int(left * r)
-
-                # draw the predicted face name on the image
-                cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)
-                y = top - 15 if top - 15 > 15 else top + 15
-                cv2.putText(frame, name, (left, y), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 255, 0), 2)
-
-            # draw the final bounding boxes
-            for (xA, yA, xB, yB) in pick:
-                cv2.rectangle(frame, (xA, yA), (xB, yB), (0, 255, 0), 2)
-
-            end_time = time.time()
-            current_fps = math.ceil(1 / np.round(end_time - start_time, 3))
-            Logger.success(f'Current FPS: {current_fps}')
+                render_buffer.task_done()
+            except queue.Empty:
+                anything_detected = False
+                if last_frame_for_server is not None:
+                    send_image_data(client, last_frame_for_server)
 
             # Present the frame
             cv2.imshow('Frame', frame)
-
-            # send the image to the server
-            send_image_data(client, frame)
 
             if cv2.waitKey(1) & 0xFF == ord("q"):
                 break
@@ -236,12 +272,16 @@ if __name__ == '__main__':
     vid.release()
     cv2.destroyAllWindows()
 
-    anythingDetected = len(names) > 0 or len(pick) > 0
-    Logger.success(f'Detected anything: {anythingDetected}')
-    leave(client, anythingDetected)
+    # wait for all the frame-putting tasks to complete:
+    for f in frame_workers:
+        f.get()
 
-    for thread in threads:
-        thread.join()
+    # signal the "show" task to end by placing None in the queue
+    detection_buffer.put(None)
+    show_process.get()
+
+    Logger.success(f'Detected anything: {anything_detected}')
+    leave(client, True)
 
     client.close()
 
